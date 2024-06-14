76420c8bc4a890b683c8f03260d96afc8c37c51a
Ravindu Jayaweera <ravindujayaweera99@gmail.com>
2024-06-14 13:26:13 +0530

    Image widths Adjusted

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -9,7 +9,7 @@ const Service = (props) => {
       <img
         src={props.img}
         alt=""
-        className="hidden md:block md:aspect-square md:w-[40vh] md:h-[35vh]"
+        className="hidden md:block md:aspect-square md:w-[40vh] md:h-[35vh] w-full"
       />
       <p className="block p-10 opacity-1 text-left rounded-none md:p-0 md:hidden md:absolute md:top-[50%] bg-blue md:bg-opacity-80 text-white md:rounded-md md:px-10 md:py-5 md:left-0 md:group-hover:block">
         {props.desc}
