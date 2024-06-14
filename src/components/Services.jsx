d1c4143f22ee4974b595b9b954e91bc0c0be34a6
Ravindu Jayaweera <ravindujayaweera99@gmail.com>
2024-06-14 13:18:50 +0530

    Added some framer motion animations:

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,12 +4,26 @@ import service3 from "../assets/service3.jpg";
 import service4 from "../assets/service4.jpg";
 import service5 from "../assets/service5.jpg";
 import Service from "./Service";
+import { motion } from "framer-motion";
 
 const Services = () => {
   return (
     <div className="h-auto mt-5 md:h-screen flex flex-col md:justify-center md:items-center md:mt-0">
       <h1 className="text-center text-3xl md:text-5xl">Our Services</h1>
-      <div className="md:container md:flex md:flex-row mx-auto justify-center items-center gap-10 md:gap-32 bg-white">
+      <motion.div
+        className="md:container md:flex md:flex-row mx-auto justify-center items-center gap-10 md:gap-32 bg-white"
+        initial={{
+          x: "-100%",
+        }}
+        whileInView={{
+          x: "0%",
+        }}
+        viewPort={{
+          margin: "-500px",
+          once: true,
+        }}
+        transition={{ duration: 1 }}
+      >
         <div className="flex flex-col justify-between items-center gap-10 md:gap-12 mt-10 md:mt-0">
           <Service
             service="Preparations of Architectural Drawings"
@@ -41,7 +55,7 @@ const Services = () => {
             desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores dolorum sit iste sapiente impedit facilis nihil. Impedit a alias eos labore quisquam itaque, quia molestias iusto minus sed laboriosam dolorum!"
           />
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
